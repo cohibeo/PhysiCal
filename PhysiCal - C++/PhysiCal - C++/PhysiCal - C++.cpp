dbdfd316d6a4e286bb03f837e47bab6c2e31d074
cohibeo <162774158+cohibeo@users.noreply.github.com>
2025-01-21 21:47:22 -0500

    0.0.1
    Created separate Java and C++ PhysiCal versions

---
+++ PhysiCal - C++/PhysiCal - C++/PhysiCal - C++.cpp
@@ -0,0 +1,12 @@
+﻿// PhysiCal - C++.cpp : Defines the entry point for the application.
+//
+
+#include "PhysiCal - C++.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
